626e6084853480ce85c3f073810283484ef2a2d6
Rayza Arasj Mahardhika <rayza.arasj@gmail.com>
2021-06-23 14:07:22 +0700

    make category clickable

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Card, CardContent, Link, Typography } from '@material-ui/core';
 import React, { FC } from 'react';
 
 type CategoryProps = {
@@ -10,7 +10,15 @@ export const Category: FC<CategoryProps> = (props: CategoryProps) => {
   return (
     <Card id={props.id.toString()}>
       <CardContent>
-        <Typography align="center">{props.category}</Typography>
+        <Typography align="center">
+          <Link
+            href={`/categories/${props.id.toString()}`}
+            color="inherit"
+            underline="none"
+          >
+            {props.category}
+          </Link>
+        </Typography>
       </CardContent>
     </Card>
   );
