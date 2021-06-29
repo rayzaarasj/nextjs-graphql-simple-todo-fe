803a878c7fe3efd3bc913835ec2b67d91dabba3d
Rayza Arasj Mahardhika <rayza.arasj@gmail.com>
2021-06-29 09:59:07 +0700

    extract category input component

---
+++ src/components/CategoryInput.tsx
@@ -0,0 +1,36 @@
+import { Container, Box, TextField, Button } from '@material-ui/core';
+import React, { FC, useState } from 'react';
+
+type CategoryInputProps = {
+  handleSubmit: (_event: React.SyntheticEvent, input: string) => void;
+  input?: string;
+};
+
+export const CategoryInput: FC<CategoryInputProps> = (
+  props: CategoryInputProps
+) => {
+  const [categoryInput, updateCategoryInput] = useState<string>(
+    props.input || ''
+  );
+
+  const handleCategoryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateCategoryInput(event.target.value);
+  };
+  return (
+    <Container>
+      <form onSubmit={(event) => props.handleSubmit(event, categoryInput)}>
+        <Container>
+          <TextField
+            label="Category"
+            fullWidth
+            onChange={handleCategoryChange}
+          />
+          <Box height="1rem" />
+          <Button type="submit" color="primary" variant="contained">
+            Create
+          </Button>
+        </Container>
+      </form>
+    </Container>
+  );
+};
