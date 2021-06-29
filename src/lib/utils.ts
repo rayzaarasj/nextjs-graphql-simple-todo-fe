78b1b67373ac21259c6ab41495465a53cb157657
Rayza Arasj Mahardhika <rayza.arasj@gmail.com>
2021-06-29 14:50:18 +0700

    fix month of formatter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,14 @@
 export function utcDateFormatter(date: Date): string {
   date.toUTCString();
   return (
-    `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()}` +
+    `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}` +
     `T${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}Z`
   );
 }
 
 export function dateInputFormatter(date: Date): string {
-  const month = date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth();
+  const month =
+    date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth();
   const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
   const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
   const minute =
