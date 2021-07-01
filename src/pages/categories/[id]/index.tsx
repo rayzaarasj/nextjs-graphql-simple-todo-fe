dde1724bbf8cec26fee41f6cc2a6172095e56408
Rayza Arasj Mahardhika <rayza.arasj@gmail.com>
2021-07-01 12:34:09 +0700

    add page title to category's todos page

--- src/pages/categories/[id]/index.tsx
+++ src/pages/categories/[id]/index.tsx
@@ -1,21 +1,34 @@
-import { Box, CircularProgress, Container } from '@material-ui/core';
+import {
+  Box,
+  CircularProgress,
+  Container,
+  Typography,
+} from '@material-ui/core';
 import { useRouter } from 'next/dist/client/router';
 import React, { ReactElement } from 'react';
 import { Todo } from '@components/Todo';
-import { useGetTodosByCategoryIdQuery } from '@generated/graphql';
+import {
+  useGetCategoryByIdQuery,
+  useGetTodosByCategoryIdQuery,
+} from '@generated/graphql';
 import { TodoType } from '@type/Todo';
 
 export default function Category(): ReactElement {
   const router = useRouter();
   const categoryId = router.query.id as string;
-  const { data, loading } = useGetTodosByCategoryIdQuery({
-    variables: {
-      id: parseInt(categoryId),
-    },
-  });
+  const { data: categoryData, loading: categoryLoading } =
+    useGetCategoryByIdQuery({
+      variables: { id: parseInt(categoryId) },
+    });
+  const { data: todosData, loading: todosLoading } =
+    useGetTodosByCategoryIdQuery({
+      variables: {
+        id: parseInt(categoryId),
+      },
+    });
 
   const todos: TodoType[] = [];
-  data?.todosByCategoryIds?.forEach((todo) => {
+  todosData?.todosByCategoryIds?.forEach((todo) => {
     todos.push({
       id: parseInt(todo.id),
       title: todo.title || '',
@@ -31,7 +44,7 @@ export default function Category(): ReactElement {
     });
   });
 
-  if (loading) {
+  if (todosLoading || categoryLoading) {
     return (
       <Box marginY="40px" display="flex" justifyContent="center">
         <CircularProgress />
@@ -41,6 +54,10 @@ export default function Category(): ReactElement {
 
   return (
     <Container>
+      <Typography variant="h1">
+        {`${categoryData?.categoryById?.category}'s Todos`}
+      </Typography>
+      <Box height="2rem" />
       {todos.map((data: TodoType, index: number) => {
         return (
           <Todo
